ec58ba423784ae1c3a69b02cb754a47d65b1fc40
Paul Wisdom <pmwisdom@gmail.com>
2015-04-25 16:09:43 -0700

    init

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'mirrorcell:smart-reload',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.1.0.2');
+
+    api.use(['reactive-var', 'reload', 'underscore']);
+    api.imply(['reactive-var', 'reload', 'underscore']);
+
+    api.addFiles('lib/smart-reload.js', 'client');
+
+    api.export('SmartReload', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mirrorcell:smart-reload');
+  api.addFiles('smart-reload-tests.js');
+});
